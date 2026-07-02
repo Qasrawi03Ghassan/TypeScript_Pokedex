6f0f0c1b99f243562e93ae7483bcbd5d04828962
Qasrawi03Ghassan <gqasrawig@gmail.com>
2026-07-02 16:41:32 +0300

    Feat: Added the main interface with a repl.log file for testing

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,25 @@
+import {createInterface} from "readline";
+
 export function cleanInput(str:string):string[]{
     let trimmed = str.trim();
     let lowered = trimmed.toLowerCase();
     let resultList = lowered.split(/\s+/);
 
     return resultList;
+}
+
+const rl = createInterface({
+    input:process.stdin,
+    output:process.stdout,
+    prompt:"Pokedex > "
+});
+export function startREPL(){
+    rl.prompt();
+    rl.on("line",(line) => {
+        if(line === '' || line === null || line === undefined)rl.prompt();
+        let words = cleanInput(line);
+        
+        console.log(`Your command was: ${words[0]}`);
+        rl.prompt();
+    });
 }
